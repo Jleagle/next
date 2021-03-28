308591cf407389355f8056e29705c604383b4c57
Jleagle <jimeagle@gmail.com>
2021-03-28 23:04:24 +0100

    Install next

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
+import "~bootstrap/bootstrap";
+// import './App.scss';
+
+function HomePage() {
+    return <div>Welcome to Next.js!</div>
+}
+
+export default HomePage
