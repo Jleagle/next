15cb04fde6034da690f3c566f964aff1fe0a894a
Jleagle <jimeagle@gmail.com>
2021-03-29 23:06:09 +0100

    Server side pagination

--- pages/table.js
+++ pages/table.js
@@ -1,44 +1,91 @@
-import DataTable from 'react-data-table-component';
-import {Component} from "react";
+import React, {useState, useEffect, useMemo} from "react";
+import axios from "axios";
+import DataTable from "react-data-table-component";
+import Link from 'next/link'
 
-const data = [
-    {
-        id: 1,
-        title: 'Conan the Barbarian',
-        year: '1982'
-    },
-    {
-        id: 2,
-        title: 'Conan the Barbarian 2',
-        year: '1983'
-    }
-];
+const AdvancedPaginationTable = () => {
 
-const columns = [
-    {
-        name: 'Title',
-        selector: 'title',
-        sortable: true,
-    },
-    {
-        name: 'Year',
-        selector: 'year',
-        sortable: true,
-        right: true,
-    },
-];
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [totalRows, setTotalRows] = useState(0);
+    const [perPage, setPerPage] = useState(10);
+    const [currentPage, setCurrentPage] = useState(1);
 
-class MyComponent extends Component {
-    render() {
-        return (
-            <DataTable
-                title="Arnold Movies"
-                columns={columns}
-                data={data}
-                sortServer={true}
-            />
-        )
-    }
+    const fetchUsers = async (page, size = perPage) => {
+
+        setLoading(true);
+
+        const response = await axios.get(`https://reqres.in/api/users?page=${page}&per_page=${size}&delay=1`);
+
+        setData(response.data.data);
+        setTotalRows(response.data.total);
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchUsers(1);
+    }, []);
+
+    const columns = useMemo(
+        () => [
+            {
+                name: "First Name",
+                selector: "first_name",
+                sortable: true
+            },
+            {
+                name: "Last Name",
+                selector: "last_name",
+                sortable: true
+            },
+            {
+                name: "Email",
+                selector: "email",
+                sortable: true
+            },
+            {
+                // eslint-disable-next-line react/button-has-type
+                cell: row => <Link href="/about">Link</Link>
+            }
+        ],
+        []
+    );
+
+    const handlePageChange = page => {
+        fetchUsers(page);
+        setCurrentPage(page);
+    };
+
+    const handlePerRowsChange = async (newPerPage, page) => {
+        fetchUsers(page, newPerPage);
+        setPerPage(newPerPage);
+    };
+
+    return (
+        <DataTable
+            title="Users"
+            columns={columns}
+            data={data}
+            progressPending={loading}
+            pagination
+            paginationServer
+            paginationTotalRows={totalRows}
+            paginationDefaultPage={currentPage}
+            onChangeRowsPerPage={handlePerRowsChange}
+            onChangePage={handlePageChange}
+            selectableRows
+            onSelectedRowsChange={({selectedRows}) => console.log(selectedRows)}
+        />
+    );
+};
+
+function Table() {
+    return (
+        <div className="App">
+            <AdvancedPaginationTable/>
+        </div>
+    );
 }
 
-export default MyComponent
+export default Table
